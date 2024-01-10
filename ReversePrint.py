b0e0befda05f0faabe3a7d3b1f4431ccee268ff1
Fernacaj <fernandes.cajetan25@gmail.com>
2024-01-10 10:43:52 +0530

    First Commit

---
+++ ReversePrint.py
@@ -0,0 +1,6 @@
+num  = 15;
+while num >= 1:
+	print (num)
+	num = num -1
+	
+	
---
+++ greatestNumber.java
@@ -0,0 +1,18 @@
+class greatestNumber
+{
+	public static void main (String args[])
+	{
+		int a, b;
+		a= Integer.parseInt(args[0]);
+		b= Integer.parseInt(args[1]);
+		
+		if(a>b)
+		{
+			System.out.println(a + "is greater");
+		}
+		else
+			{
+				System.out.println(b + "is greater");
+				}
+			}	
+		}	
